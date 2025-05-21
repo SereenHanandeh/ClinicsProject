315ae4f84d4ab768b96892b5d0f42b2837313a17
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-05-21 21:13:33 +0300

    add routes

--- clinicSystem/src/app/app.routes.ts
+++ clinicSystem/src/app/app.routes.ts
@@ -1,4 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { authGuard } from './core/guards/Auth/auth.guard';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }];
+export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  { path: '**', component: NotFoundComponent },
+];
