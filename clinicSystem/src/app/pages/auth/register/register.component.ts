e80e9fbb0ec5d7119d07a0cbc2754f7527f73774
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-05-21 21:10:11 +0300

    register

---
+++ clinicSystem/src/app/pages/auth/register/register.component.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../core/services/Auth/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+ registerForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        phone: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(/^\d{3} ?\d{3} ?\d{4}$/),
+          ],
+        ],
+        gender: ['male', [Validators.required]],
+        dateOfBirth: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+        userType: ['patient'],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onPhoneInput(event: any) {
+    let input = event.target.value.replace(/\D/g, '');
+    if (input.length > 10) input = input.slice(0, 10);
+    let formatted = input;
+    if (input.length > 3) {
+      formatted = input.slice(0, 3) + ' ';
+      if (input.length > 6) {
+        formatted += input.slice(3, 6) + ' ' + input.slice(6);
+      } else {
+        formatted += input.slice(3);
+      }
+    }
+    this.registerForm.get('phone')?.setValue(formatted, { emitEvent: false });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.errorMessage = 'Please fill all fields correctly.';
+      return;
+    }
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.successMessage = 'Registration successful!';
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        this.errorMessage = error.message || 'Registration failed.';
+      },
+    });
+  }
+}
