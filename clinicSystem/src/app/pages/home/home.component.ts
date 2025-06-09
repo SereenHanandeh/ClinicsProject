49915b7e647df3e4eecd01f633ea3d6055f30c87
Hussein Tirawi <hmtirawi@gmail.com>
2025-06-10 01:25:24 +0300

    fixed translations

--- clinicSystem/src/app/pages/home/home.component.ts
+++ clinicSystem/src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '../../shared/pips/translate.pipe';
+import { I18nService } from '../../core/services/i18n/i18n.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [TranslatePipe, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  currentLang: 'en' | 'ar' = 'en';
+  currentYear = new Date().getFullYear();
 
+  constructor(private i18n: I18nService) {
+    this.currentLang = this.i18n.getLanguage();
+  }
+
+  switchLang(lang: 'en' | 'ar') {
+    console.log(lang);
+    this.i18n.loadTranslations(lang);
+    this.i18n.setLanguage(lang);
+    this.currentLang = lang;
+  }
 }
