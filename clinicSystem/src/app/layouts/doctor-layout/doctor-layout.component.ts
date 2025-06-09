32a28b48e6b5b510ec1d62d22707724850040804
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-06-10 01:07:03 +0300

    add OTHER

--- clinicSystem/src/app/layouts/doctor-layout/doctor-layout.component.ts
+++ clinicSystem/src/app/layouts/doctor-layout/doctor-layout.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from "../../shared/pips/translate.pipe";
+import { FooterComponent } from "../../pages/footer/footer.component";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-doctor-layout',
-  imports: [RouterModule, TranslatePipe],
+  imports: [RouterModule, TranslatePipe, FooterComponent,TranslateModule],
   templateUrl: './doctor-layout.component.html',
   styleUrl: './doctor-layout.component.scss',
 })
-export class DoctorLayoutComponent {}
+export class DoctorLayoutComponent {
+    private translate = inject(TranslateService);
+
+     switchLanguage(lang: string) {
+    this.translate.use(lang);
+    document.documentElement.lang = lang;
+    document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  }
+
+}
